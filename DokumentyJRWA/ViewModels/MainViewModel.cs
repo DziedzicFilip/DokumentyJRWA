4934e072e7cb681a9eaeba30646e6bfd216a2b9c
DziedzicFilip <dziedzicfilip00@gmail.com>
2026-01-10 19:40:06 +0100

    Widok głowny dodanie menu boczne

--- DokumentyJRWA/ViewModels/MainViewModel.cs
+++ DokumentyJRWA/ViewModels/MainViewModel.cs
@@ -6,6 +6,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using DokumentyJRWA.Commands;
 
 namespace DokumentyJRWA.ViewModels
 {
@@ -14,11 +16,31 @@ namespace DokumentyJRWA.ViewModels
         public readonly DocumentService _documentService = new();
 
         public ObservableCollection<Dokument> Dokumenty { get;}
+        
+        // Commands
+        public ICommand AddFileCommand { get; }
+        public ICommand ExportImportArchitectureCommand { get; }
+
         public MainViewModel()
         {
             Dokumenty = new ObservableCollection<Dokument> (
                     _documentService.GetAll()
                 );
+
+            AddFileCommand = new RelayCommand(ExecuteAddFile);
+            ExportImportArchitectureCommand = new RelayCommand(ExecuteExportImportArchitecture);
+        }
+
+        private void ExecuteAddFile(object? obj)
+        {
+            // Logic for adding a new file will go here
+            System.Windows.MessageBox.Show("Dodaj nowy plik - Functionality not implemented yet.");
+        }
+
+        private void ExecuteExportImportArchitecture(object? obj)
+        {
+            // Logic for Export/Import Architecture will go here
+            System.Windows.MessageBox.Show("Export/Import Architektury JWRA - Functionality not implemented yet.");
         }
     }
 }
